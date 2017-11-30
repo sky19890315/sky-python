00a57f96318f4c510d837487ac39d5065660bff0
alex <alex@gmail.com>
2017-11-30 18:20:05 +0800

    修正相关

--- kenblog/app/__init__.py
+++ kenblog/app/__init__.py
@@ -2,3 +2,10 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from config import basedir
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from app import views
--- kenblog/config.py
+++ kenblog/config.py
@@ -1,1 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'sky.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_sky_repository')
+
+CSRF_ENABLED = True
+SECRET_KEY = 'this is * can % try ^!'
