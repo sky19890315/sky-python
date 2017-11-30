66cc14cb34aac49a390a9b5b255c2f117b76bd45
alex <alex@gmail.com>
2017-11-30 18:32:08 +0800

    blog

--- kenblog/app/views.py
+++ kenblog/app/views.py
@@ -1,7 +1,27 @@
-from flask import render_tmplate
+# from flask import render_tmplate
 from app import app
 
 
 @app.route('/')
 def index():
     return 'Hello world'
+
+
+@app.route('/about')
+def about():
+    return 'about ken!'
+
+
+@app.route('/contact')
+def contact():
+    return 'contact me!'
+
+
+@app.route('/home')
+def home():
+    return 'Home'
+
+
+@app.route('/login')
+def login():
+    return 'Login!'
