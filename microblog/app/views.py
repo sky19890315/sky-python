14fbc5c1322195ac0cdd777c89718d2ba0b8ca96
alex <alex@gmail.com>
2017-11-30 17:03:45 +0800

    增加变动

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,12 +1,22 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
+from .models import User
+
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'nickname': 'ken'}
+    user = g.user
+    # user = {'nickname': 'ken'}
     posts = [
         {
             'author': {'nickname': 'ken win'},
@@ -22,14 +32,68 @@ def index():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenId="' + form.openid + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+        # flash('Login requested for OpenId="' + form.openid + '", remember_me=' + str(form.remember_me.data))
+        # return redirect('/index')
     return render_template('login.html', title='Sign In', form=form,
-                           providers = app.config['OPENID_PROVIDERS'])
+                           providers=app.config['OPENID_PROVIDERS'])
 
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again!')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == '':
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    load_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User' + nickname + 'Not found!')
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #01'},
+        {'author': user, 'body': 'Test post #02'},
+        {'author': user, 'body': 'Test post #03'},
+        {'author': user, 'body': 'Test post #04'},
+        {'author': user, 'body': 'Test post #05'},
+        {'author': user, 'body': 'Test post #06'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
 
 
 
